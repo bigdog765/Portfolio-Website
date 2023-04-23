6219fa2a3e71ae33659e12a2c93979b76159b5a3
bigdog765 <aidy.vg@gmail.com>
2023-04-23 14:51:07 -0400

    more work

--- portf/src/App.js
+++ portf/src/App.js
@@ -8,22 +8,33 @@ import Nav from './Nav';
 
 export default function MyApp() {
   return (
-    <div className='App'>
-        <div className='NavBar'>
-          <Nav title='Aidans Portfolio'>
+    <div className='background-sh'>
+      <div className='App'>
+          <div className='NavBar'>
+            <Nav title='Aidans Portfolio'>
 
-          </Nav>
-        </div>
-        <div className='Cards'>
-          <Card 
-          title='Software Development'
-          imgUrl={img1}
-          body='Hello this is the body text.'/>
-          
-        </div>
-    
+            </Nav>
+          </div>
+          <div className='Cards'>
+            <Card 
+            title='Software Development'
+            imgUrl={img1}
+            body='Hello this is the body text.'/>
+
+            <Card 
+            title='Game Development'
+            imgUrl={img1}
+            body='Hello this is the body text.'/>
+
+            <Card 
+            title='Creative Work'
+            imgUrl={img1}
+            body='Hello this is the body text.'/>
+            
+          </div>
+      
+      </div>
     </div>
-    
     
     
   );
--- portf/src/Card.js
+++ portf/src/Card.js
@@ -4,20 +4,24 @@ import './Card.css'
 
 function Card({title, imgUrl, body}){
     return(
-        <div className='card-container'>
-            <div className='img-container'>
-                <img src={imgUrl} className='imgcss' alt=''></img>
-            </div>
-            <div className='card-content'>
-                <div className='card-title'>
-                    <h3>{title}</h3>
+        <div className='card-parent'>
+            <div className='card-container'>
+                <div className='img-container'>
+                    <img src={imgUrl} className='imgcss' alt=''></img>
                 </div>
-                <div className='card-body'>
-                    <p>{body}</p>
+                <div className='card-content'>
+                    <div className='card-title'>
+                        <h3>{title}</h3>
+                    </div>
+                    <div className='card-body'>
+                        <p>{body}</p>
+                    </div>
                 </div>
             </div>
             
         </div>
+        
+        
     )
 }
 export default Card
