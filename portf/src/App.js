8198da28da91b63ef69a21b7543d7a95b15f4c67
bigdog765 <aidy.vg@gmail.com>
2023-04-30 13:31:40 -0400

    pictures

--- portf/src/App.js
+++ portf/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card.tsx'
-import img1 from './assets/dalle-code.png'
+import image_code from './assets/dalle-code.png'
+import image_game from './assets/dalle-videogame.png'
+import image_art from './assets/dalle-art.png'
 import Nav from './Nav';
 
 
@@ -18,18 +20,18 @@ export default function MyApp() {
           <div className='Cards'>
             <Card 
             title='Software Development'
-            imgUrl={img1}
-            body='Hello this is the body text.'/>
+            imgUrl={image_code}
+            body='Work that shows off the talent of my programming skills containing applications, scripts and more.'/>
 
             <Card 
             title='Game Development'
-            imgUrl={img1}
-            body='Hello this is the body text.'/>
+            imgUrl={image_game}
+            body="A section dedicated to the enviromental art and game logic I've designed for various video game projects."/>
 
             <Card 
             title='Creative Work'
-            imgUrl={img1}
-            body='Hello this is the body text.'/>
+            imgUrl={image_art}
+            body="An assortment of artistic creations across different forms and styles."/>
             
           </div>
       
