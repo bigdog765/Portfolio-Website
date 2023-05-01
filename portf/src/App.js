15e0e9ee40762b22bfc63268d2e01529f204ac20
bigdog765 <aidy.vg@gmail.com>
2023-05-01 14:57:41 -0400

    bottom work

--- portf/src/App.js
+++ portf/src/App.js
@@ -4,6 +4,8 @@ import Card from './Card.tsx'
 import image_code from './assets/dalle-code.png'
 import image_game from './assets/dalle-videogame.png'
 import image_art from './assets/dalle-art.png'
+import img_wayne from './assets/wayne-logo.png'
+import img_code from './assets/programming-icon.png'
 import Nav from './Nav';
 
 
@@ -34,12 +36,33 @@ export default function MyApp() {
             body="An assortment of artistic creations across different forms and styles."/>
             
           </div>
-          <div className='paragraph1'>
-            Greetings! My name is Aidan Van Gessel, and I am a Computer Science graduate from Wayne State University. I am deeply passionate about the art of developing effective and innovative software programs. As a level-headed worker, I am constantly seeking opportunities to enhance my personal and career skills.
-              My programming skills are primarily focused on object-oriented languages such as C++ and Java. Additionally, I am experienced in working with Python, SQL, JavaScript, and HTML, allowing me to take on a wide range of programming tasks with confidence.
+          <div className='text-section1'>
+            <div className='paragraph1'>
+              Greetings! My name is <b>Aidan Van Gessel</b>, and I am a Computer Science graduate from <b>Wayne State University</b>. 
+              I am deeply passionate about the art of developing unique and innovative software programs. As a <b>level-headed worker</b>, 
+              I am constantly seeking opportunities to enhance my personal and career skills.
+              My programming skills are primarily focused on object-oriented languages such as <b>C++</b> and <b>Java</b>. 
+              Additionally, I am experienced in working with Python, SQL, JavaScript, and HTML, allowing me to take on a wide range of 
+              programming tasks with confidence.
+            </div>
+            <div className='icon-container1'>
+              <div className='wayne-icon'>
+                <img src={img_wayne} className='imgcss-wayne' alt=''></img>
+              </div>
+              <div className='code-icon'>
+                <img src={img_code} className='imgcss-code' alt=''></img>
+              </div>
+            </div>
           </div>
+          
           <div className='paragraph2'>
-            Throughout my career, I have developed a diverse skill set that has allowed me to take on a wide range of professional projects. I have worked on everything from game development to graphic design to database management, and each project has challenged me to grow both personally and professionally.
+            Throughout my academic and professional career, I have developed a diverse skill set that has allowed me to take on a wide range of professional projects. 
+            I have worked on everything from <b>game development</b> to <b>RESTful web services</b> to <b>database management</b>, and each project has challenged me to 
+            grow both personally and professionally.
+          </div>
+
+          <div className='bottom-nav'>   
+            aidanvg.live
           </div>
       
       </div>
