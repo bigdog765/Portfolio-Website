605880b1b56b1a23bc02f5eca4b9424abafa7405
Aidan <aidy.vg@gmail.com>
2025-03-17 17:33:33 -0400

    css

--- portf/src/Card.tsx
+++ portf/src/Card.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import './Card.css'
 import RenderModel from './RenderModel.tsx'
+import { Link } from 'react-router-dom'
 
 
 function Card({title, imgUrl, body}: {title:string,imgUrl:string,body:string}){
     const isModel = imgUrl.includes('.glb')
     return(
         <div className='card-parent'>
-            <div className='card-container'>
-                <div className='img-container'>
-                    {isModel ? <RenderModel modelPath={imgUrl} width={300} height={300} /> :
-                    <img src={imgUrl} className='imgcss' alt=''></img>}
+            
+                <div className='card-container'>
+                    <Link to='/work'>
+                        <div className='img-container'>
+                            {isModel ? <RenderModel modelPath={imgUrl}  /> :
+                            <img src={imgUrl} className='imgcss' alt=''></img>}
+                        </div>
+                        <div className='card-content'>
+                            <div className='card-title'>
+                                <h3>{title}</h3>
+                            </div>
+                            <div className='card-body'>
+                                <p>{body}</p>
+                            </div>
+                        </div>
+                    </Link>
                 </div>
-                <div className='card-content'>
-                    <div className='card-title'>
-                        <h3>{title}</h3>
-                    </div>
-                    <div className='card-body'>
-                        <p>{body}</p>
-                    </div>
-                </div>
-            </div>
             
         </div>
         
