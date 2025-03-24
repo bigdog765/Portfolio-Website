ad15ef74b5f09cd57cd829f146aeda7300239ed5
Aidan <aidy.vg@gmail.com>
2025-03-24 17:14:32 -0400

    yur

--- portf/src/RenderModel.tsx
+++ portf/src/RenderModel.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef} from "react";
 import * as THREE from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const loader = new GLTFLoader();
 
 const RenderModel = ({modelPath, width=window.innerWidth, height=window.innerHeight}) => {
@@ -16,6 +18,7 @@ const RenderModel = ({modelPath, width=window.innerWidth, height=window.innerHei
     renderer.setClearColor(0x2e2e2e, 1); // Set background color to dark grey
     
     mountRef.current.appendChild(renderer.domElement);
+    setOrbit(camera, renderer);
 
     loader.load(modelPath, function (gltf) {
       setLighting();
@@ -26,14 +29,19 @@ const RenderModel = ({modelPath, width=window.innerWidth, height=window.innerHei
       renderer.setAnimationLoop( animate );
 
       model.current.scale.set(0.5, 0.5, 0.5);
-      model.current.position.set(0, -2, 0);
+      model.current.position.set(0, -2.25, 0);
       camera.position.z = 3.5;
     }, undefined, function (error) {
       console.error(error);
     });
     
     function setLighting(){
-      const light = new THREE.AmbientLight( 0x404040, 50 ); // soft white light
+      const light = new THREE.AmbientLight( 0x404040, 30 ); // soft white light
+      // White directional light at half intensity shining from the top.
+      const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+      directionalLight.position.set(0, 5, 5);
+      
+      scene.add( directionalLight );
       scene.add( light );
     }
     function setPivot(model: any) {
@@ -44,9 +52,16 @@ const RenderModel = ({modelPath, width=window.innerWidth, height=window.innerHei
 
     function animate() {
       //model.current.rotation.x += 0.01;
-      model.current.rotation.y += 0.003;
+      //model.current.rotation.y += 0.003;
       renderer.render(scene, camera);
     };
+    function setOrbit(camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) {
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.update(); // Required for damping
+      controls.enableDamping = true;
+      controls.dampingFactor = 0.05;
+      controls.zoomSpeed = 1.2;
+    }
     
     return () => {
       if( mountRef.current) {
@@ -60,5 +75,7 @@ const RenderModel = ({modelPath, width=window.innerWidth, height=window.innerHei
 };
   
 export default RenderModel;
+
+
 
 
